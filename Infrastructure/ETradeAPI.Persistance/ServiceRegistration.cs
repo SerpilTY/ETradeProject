de757b272285522712842650cbe0ab0567a0cd6b
Serpil <serpilturanyuksel@gmail.com>
2023-06-01 17:19:03 +0300

    Add project files.

---
+++ Infrastructure/ETradeAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using ETradeAPI.Application.Abstraction;
+using ETradeAPI.Persistance.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ETradeAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection service)
+        {
+            service.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
